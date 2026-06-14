580fd00f6fa8825308c019cba6793e9d5150b2c5
Rajneesh Raj <rajneesh.raj.0103@gmail.com>
2026-06-14 17:58:38 +0530

    Buttons Basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+const BooleanCheck = () => {
+  const boolean  = true;
+  return(
+    <div className={`${boolean ? "bg-orange-600" : "bg-purple-600"} text-white`}>
+    ToDo!
+    </div>  
+  )
+}
+
 const App = () => {
   return <div>
-    <div className="text-rose-300 bg-red-700">One</div>
-    <div className="text-slate-400 border-2 border-slate-600">Two</div>
-    <div className="text-2xl bg-emerald-200 text-blue-600">Three</div>
-
-    <div className="text-xl text-purple-500" >
-      <div className=" border-4 border-sky-500">Four</div>
-      <div className="bg-violet-200 border-l-8 border-purple-800">Five</div>
-    </div> 
+    <BooleanCheck/>
+    <button className="border-2 border-green-600 text-green-400 px-8 py-4 m-4 rounded-md">
+      first
+    </button>
+    <button className="rounded-lg bg-sky-400 text-white px-10 py-4 text-3xl hover:text-base m-4">
+      second
+    </button>
+    <button className="bg-violet-100 text-violet-600 px-8 py-2 rounded-full m-4">
+      third
+    </button>
+    <button className="m-4 bg-cyan-700 text-cyan-200 p-10 rounded-lg hover:bg-cyan-200 hover:text-cyan-700">
+      four
+    </button>
   </div>
 };
 
