945fca2e2cdeaac3f9639c05954f51f32856c2d8
Rajneesh Raj <rajneesh.raj.0103@gmail.com>
2026-06-28 14:43:08 +0530

    Build a light switch app using React state and clsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import {useState} from "react";
+import {clsx} from "clsx";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
+  const [isOn, setIsOn] = useState(true);
+  
   return (
-    <div className="text-4xl flex justify-center border border-black h-screen items-center">
-      <button 
-      className="text-white w-8 rounded-md bg-red-400"
-      onClick={() => setCount(count - 1)}
-      >-</button>
+    <div 
+    className={clsx(
+      "flex justify-center items-center h-screen",
+      isOn ? "bg-yellow-100" : "bg-slate-600"
+    )}>
+      <button onClick={() => setIsOn(!isOn)}>
+      <div
+        className={clsx(
+          "h-8 w-6",
+          isOn ? "bg-slate-300" : "bg-slate-200"
+        )}
+      ></div>
 
-      <div className="m-4 text-neutral-400 w-20 text-center">{count}</div>
-
-      <button 
-      className="text-white w-8 rounded-md bg-emerald-400"
-      onClick={() => setCount(count + 1)}
-      >+</button>
+      <div
+        className={clsx(
+          "h-8 w-6",
+          isOn ? "bg-slate-200" : "bg-slate-300"
+        )}
+      ></div>
+    </button>
     </div>
   );
 };
