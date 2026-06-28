9b4685be03be8e02ca02e96134a0e579eed746cb
Rajneesh Raj <rajneesh.raj.0103@gmail.com>
2026-06-28 13:54:56 +0530

    Build a counter app using React useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import Widget1 from "./Widget1";
-import Widget2 from "./Widget2";
-import Widget3 from "./Widget3";
+import {useState} from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col items-center">
-      <Widget1 />
-      <Widget2 />
-      <Widget3 />
+    <div className="text-4xl flex justify-center border border-black h-screen items-center">
+      <button 
+      className="text-white w-8 rounded-md bg-red-400"
+      onClick={() => setCount(count - 1)}
+      >-</button>
+
+      <div className="m-4 text-neutral-400 w-20 text-center">{count}</div>
+
+      <button 
+      className="text-white w-8 rounded-md bg-emerald-400"
+      onClick={() => setCount(count + 1)}
+      >+</button>
     </div>
   );
 };
